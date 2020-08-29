4cb27c222e9b2a61f2c486ab96f313e76e365c3c
chencichang <cichang.chen@verycloud.cn>
2020-08-29 22:50:38 +0800

    修改代码，完善下载文件

--- xiaohua/spiders/xiaohuar.py
+++ xiaohua/spiders/xiaohuar.py
@@ -2,14 +2,14 @@
 '''
 程序运行说明
 在xiaohua目录下运行
-scrapy crawl xiaohua
-会失败
-而再spiders目录下运行
+scrapy crawl xiaohuar
+再spiders目录下运行
 scrapy runspider xiaohuar.py
-则会成功
+会成功
 稍后来改正在pycharm下也能运行
 '''
 import scrapy
+from urllib import parse
 from xiaohua.items import XiaohuaItem
 
 
@@ -25,7 +25,8 @@ class XiaohuarSpider(scrapy.Spider):
         li_list = response.xpath('//*[@id="content"]//div[@class="index_img list_center"]/ul/li')
         for li in li_list:
             # 匹配图片的地址,图片名称
-            img_url = self.base_url + li.xpath('.//img/@src').extract_first()
+            img_src = li.xpath('.//img/@src').extract_first() # 获取相对路径
+            img_url = parse.urljoin(response.url, img_src) # 拼接成完整的url路径
             img_name = li.xpath('.//img/@alt').extract_first() + '.jpg'
             item = XiaohuaItem() # 实例化对象
             # 将数据封装到item中，这里只能用['img_ur']的形式，不能用点的方式
@@ -34,6 +35,13 @@ class XiaohuarSpider(scrapy.Spider):
             yield item
 
         # 拼接页码，并递归调用自己，达到处理所有页码的目的
-        for i in range(1, 12):
-            new_url = self.urls % i
-            yield scrapy.Request(url=new_url, callback=self.parse)
+        # 这里修改为是否能获取到下一页
+        next_page = response.xpath('//div[@id="content"]//div[@class="listpage"]//ol//li//a[contains(string(), "下一页")]')
+        # print(next_page)
+        if next_page is not None:
+            next_href = next_page.xpath('.//@href').get() # 获取相对路径
+            next_page_url = parse.urljoin(response.url, next_href) # 拼接成完整的url路径
+            print(next_page_url)
+            yield scrapy.Request(url=next_page_url, callback=self.parse)
+        else:
+            print('emptydddddddddddd')
